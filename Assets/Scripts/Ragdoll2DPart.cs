3502e30b568fe9c6110677a7961b332faf91e7f9
joansolroo <solroo@gmail.com>
2018-12-01 17:54:58 +0100

    Revert "ragdoll nicer physics"
    This reverts commit 88b30f88ba0509e25376824408ba3389a86d58bc.

--- Assets/Scripts/Ragdoll2DPart.cs
+++ Assets/Scripts/Ragdoll2DPart.cs
@@ -7,8 +7,6 @@ public class Ragdoll2DPart : MonoBehaviour {
     Ragdoll2D ragdoll;
     HashSet<Transform> hits = new HashSet<Transform>();
 
-    [SerializeField] float health = 100;
-    [SerializeField] float armor = 30;
     private void Start()
     {
         ragdoll = transform.parent.gameObject.GetComponent<Ragdoll2D>();
@@ -17,26 +15,9 @@ public class Ragdoll2DPart : MonoBehaviour {
     {
         if(collision.transform.parent!=this.transform.parent)
         {
-            this.GetComponent<Rigidbody2D>().AddForce(collision.relativeVelocity);
-
             Debug.Log("hit:" + this.name+" to "+collision.gameObject.name);
             hits.Add(collision.transform);
             ragdoll.CollisionEnter(this, collision);
-            float dmg = collision.relativeVelocity.magnitude * 0.5f;
-            if (dmg > armor)
-            {
-                health -= dmg;
-                this.gameObject.GetComponent<SpriteRenderer>().color = Color.Lerp(Color.red, Color.white, health / 100f);
-                if (health <= 0)
-                {
-                    HingeJoint2D joint = GetComponent<HingeJoint2D>();
-                    if (joint != null)
-                    {
-                        joint.enabled = false;
-
-                    }
-                }
-            }
         }
     }
 
