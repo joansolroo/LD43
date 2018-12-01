6f0ed8b0c801a811b1254cfd0e594d092cc65760
joansolroo <solroo@gmail.com>
2018-12-01 15:32:26 +0100

    Ragdoll collision detection

---
+++ Assets/Scripts/Ragdoll2D.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ragdoll2D : MonoBehaviour {
+
+    public  Ragdoll2DPart[] parts;
+
+	// Use this for initialization
+	void Start () {
+        parts = new Ragdoll2DPart[this.transform.childCount];
+        for(int c = 0; c< transform.childCount; ++c)
+        {
+            parts[c] = transform.GetChild(c).GetComponent<Ragdoll2DPart>();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Ragdoll2DPart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ragdoll2DPart : MonoBehaviour {
+
+    HashSet<Transform> hits = new HashSet<Transform>();
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.parent!=this.transform.parent)
+        {
+            Debug.Log("hit:" + this.name+" to "+collision.gameObject.name);
+            hits.Add(collision.transform);
+        }
+        
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.transform.parent != this.transform.parent)
+        {
+            Debug.Log("hit out:" + this.name + " to " + collision.gameObject.name);
+            hits.Remove(collision.transform);
+        }
+    }
+
+}
+
