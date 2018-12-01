9bc650ab28efdb6392664885a19f8e8fb56f5c7c
Thibault <thibault.laine@inria.fr>
2018-12-01 17:51:57 +0100

    end splashscreen

--- Assets/Scripts/GameSystem.cs
+++ Assets/Scripts/GameSystem.cs
@@ -1,28 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameSystem : MonoBehaviour {
 
-    public float gameDuration = 60.0f;
+    public float gameDuration;
     public bool pause;
 
     public GameObject player;
-    public Canvas gameUI;
-    public Canvas stopUI;
+    public GameObject gameUI;
+    public GameObject stopUI;
+
+    public Text scoreDisplay;
+    public Button retry, quit;
 
     // Use this for initialization
-    void Start () {
+    void Start() {
         pause = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        gameUI.SetActive(true);
+        stopUI.SetActive(false);
+
+        quit.onClick.AddListener(QuitGame);
+        retry.onClick.AddListener(RetryLevel);
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
+    }
+    void RetryLevel()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    // Update is called once per frame
+    void Update () {
         if(gameDuration < 0 && !pause)
         {
             pause = true;
-            gameUI.enable = false;
-            stopUI.enable = false;
+            gameUI.SetActive(false);
+            stopUI.SetActive(true);
+
+            scoreDisplay.text = ((int)(player.GetComponent<controller>().score)).ToString();
+
             player.GetComponent<controller>().enabled = false;
         }
         else if (gameDuration >= 0.0f)
