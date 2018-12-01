8451e6d3320c1654fa8b8221fa650e39465ed140
Thibault <thibault.laine@inria.fr>
2018-12-01 19:07:01 +0100

    Circus scene draft

---
+++ Assets/Scripts/Pendulum.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pendulum : MonoBehaviour {
+
+    private float t;
+    public float speed;
+    public float angle;
+
+    // Use this for initialization
+    void Start () {
+        t = 0.0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        t += speed * Time.deltaTime;
+        transform.eulerAngles = new Vector3(0, 0, angle * Mathf.Sin(t));
+	}
+}
