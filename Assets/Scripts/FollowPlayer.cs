639b700a8c66077866c066ab654cf90e829e358a
Thibault <thibault.laine@inria.fr>
2018-12-02 14:11:14 +0100

    story level

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -14,9 +14,11 @@ public class FollowPlayer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update ()
-    {
+    {/*
         float x = Mathf.Clamp(player.position.x, background.position.x - 0.5f * background.localScale.x, background.position.x + 0.5f * background.localScale.x);
         float y = Mathf.Clamp(player.position.y, background.position.y - 0.5f * background.localScale.y, background.position.y + 0.5f * background.localScale.y);
         transform.position = new Vector3(x, y, transform.position.z);
+        */
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
     }
 }
